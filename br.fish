6505d7217b6580dc2950eff0eb1aaef70c677bf3
nimaaskarian <nimaaskarian.work@outlook.com>
2025-06-21 12:11:04 +0330

    Add glow option, Improve name resolving

--- br.fish
+++ br.fish
@@ -3,6 +3,7 @@ function fd_br_vault
 end
 complete -f -c br -d "Edit named entry" -s 'n' -a "(fd_br_vault)" -r
 complete -f -c br -d "Use PAGER" -s 'l'
+complete -f -c br -d "Use glow -p" -s 'g'
 complete -f -c br -d "Use \"echo\"(print the name of the file)" -s 'p'
 complete -f -c br -d "Edit entry as encrypted" -s 'e'
 complete -F -c br -d "Load another config file" -s 'c'
