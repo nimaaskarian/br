c6a9098bf8ba9f7e3241f08ff518f25c8ba16633
nimaaskarian <nimaaskarian.work@outlook.com>
2025-06-22 07:20:44 +0330

    Add cat option

--- br.fish
+++ br.fish
@@ -6,7 +6,7 @@ complete -f -c br -d "Use PAGER" -s 'l'
 complete -f -c br -d "Use glow -p" -s 'g'
 complete -f -c br -d "Use \"echo\"(print the name of the file)" -s 'p'
 complete -f -c br -d "Edit entry as encrypted" -s 'e'
-complete -F -c br -d "Load another config file" -s 'c'
+complete -f -c br -d "Use cat" -s 'c'
 complete -F -c br -d "Br vault" -s 'v'
 set -l subcmds (br lscmd)
 complete -c br -a "$subcmds" -n "not __fish_seen_subcommand_from $subcmds" -f
