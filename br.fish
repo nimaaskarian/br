f6f36bfd6fb05b1d773fb772d998dd0d6296e3cc
nimaaskarian <nimaaskarian.work@outlook.com>
2025-06-27 10:25:36 +0330

    Update file and name handling

--- br.fish
+++ br.fish
@@ -1,11 +1,5 @@
-function fd_br_vault
-  fd --base-directory ~/Documents/br-vault | sed 's/\..*//'
-end
-function fd_br_tags
-  fd -td --base-directory ~/Documents/br-vault/.tags | sed 's/\..*//'
-end
-complete -f -c br -d "Edit named entry" -s 'n' -a "(fd_br_vault)" -r
-complete -f -c br -d "Select a tag" -s 't' -a "(fd_br_tags)" -r
+complete -f -c br -d "Edit named entry" -s 'n' -a "(fd --base-directory ~/Documents/br-vault)" -r
+complete -f -c br -d "Select a tag" -s 't' -a "(fd -td --base-directory ~/Documents/br-vault/.tags)" -r
 complete -f -c br -d "Use PAGER" -s 'l'
 complete -f -c br -d "Use glow -p" -s 'g'
 complete -f -c br -d "Use \"echo\"(print the name of the file)" -s 'p'
